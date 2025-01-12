e2eff8d8cde1e669299ac9e21dc395bbe465f34d
MuradA00 <murad.abyshly@cowchain.io>
2025-01-12 02:58:41 +0200

    done

--- source/js/main.js
+++ source/js/main.js
@@ -4,22 +4,24 @@ import './_vendor';
 
 
 const menuButton = document.querySelector('.header-menu');
-const menu = document.querySelector('.nav');
-const closeMenuButton = document.querySelector('.nav-close')
-const streamItems = document.querySelectorAll('.streams-item');
+const menu = document.querySelector('.header-body_desktop');
+const tabsControllers = document.querySelectorAll(".news-filter__btn");
+const tabsContents = document.querySelectorAll(".news-content");
+const closeMenuButton = document.querySelector('.header-close')
 const body = document.body;
 
-if (streamItems.length > 0) {
-  streamItems.forEach(item => {
-    const playButton = item.querySelector('.streams-play');
-    const preview = item.querySelector('.streams-item__preview');
-    const video = item.querySelector('.streams-item__video');
+if (tabsControllers.length) {
+  tabsControllers.forEach((controller, i) => {
+    controller.addEventListener("click", () => {
+      if (controller.classList.contains("button--purple")) return;
 
-    playButton.addEventListener('click', () => {
-      preview.style.opacity = 0;
-      video.style.opacity = 1;
-      playButton.style.transform = "translate(-50%, -50%), scale(.95)"
-      playButton.style.opacity = 0;
+      tabsControllers.forEach(
+        controller => controller.classList.remove("button--purple")
+      );
+      tabsContents.forEach(content => content.classList.remove("fade-in"));
+
+      controller.classList.add("button--purple");
+      tabsContents[i].classList.add("fade-in");
     })
   })
 }
@@ -48,11 +50,11 @@ if (streamItems.length > 0) {
 const menuHandler = () => {
   menuButton.classList.toggle('header-menu--active');
   if (menuButton.classList.contains('header-menu--active')) {
-    menu.classList.add('nav--active')
+    menu.classList.add('header-body_desktop--active')
     document.documentElement.style.overflow = 'hidden';
     body.style.overflow = 'hidden'
   } else {
-    menu.classList.remove('nav--active')
+    menu.classList.remove('header-body_desktop--active')
     document.documentElement.style.overflow = '';
     body.style.overflow = ''
   }
@@ -63,7 +65,7 @@ if (menuButton) {
 
   closeMenuButton.addEventListener('click', () => {
     menuButton.classList.remove('header-menu--active');
-    menu.classList.remove('nav--active')
+    menu.classList.remove('header-body_desktop--active')
     document.documentElement.style.overflow = '';
     body.style.overflow = ''
   })
@@ -71,6 +73,25 @@ if (menuButton) {
 
 
 if (Swiper) {
+  const newsSliderProps = {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      }
+    },
+  }
+
   new Swiper('.faq-inner', {
     speed: 300,
     slidesPerView: 1,
@@ -95,23 +116,28 @@ if (Swiper) {
     }
   })
 
-  new Swiper(".news-slider", {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    breakpoints: {
-      500: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
-      1200: {
-        slidesPerView: 4,
-        spaceBetween: 50,
-      }
-    },
+  new Swiper(".news-content--all .news-slider", {
+    ...newsSliderProps,
+    navigation: {
+      nextEl: ".news-content--all .news-controls__btn--next",
+      prevEl: ".news-content--all .news-controls__btn--prev"
+    }
+  })
+
+  new Swiper(".news-content--news .news-slider", {
+    ...newsSliderProps,
+    navigation: {
+      nextEl: ".news-content--news .news-controls__btn--next",
+      prevEl: ".news-content--news .news-controls__btn--prev"
+    }
+  })
+
+  new Swiper(".news-content--events .news-slider", {
+    ...newsSliderProps,
+    navigation: {
+      nextEl: ".news-content--events .news-controls__btn--next",
+      prevEl: ".news-content--events .news-controls__btn--prev"
+    }
   })
 }
 
